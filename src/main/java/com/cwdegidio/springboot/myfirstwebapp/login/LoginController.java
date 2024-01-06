2f540da67c421c0d90227e1ba9e51a2de0d9a7c4
Curtis W. DeGidio <curtis.degidio@gmail.com>
2024-01-05 23:29:18 -0600

    Added welcome jsp, updated login path for POST handling, added form to login jsp

--- src/main/java/com/cwdegidio/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/cwdegidio/springboot/myfirstwebapp/login/LoginController.java
@@ -1,23 +1,24 @@
 package com.cwdegidio.springboot.myfirstwebapp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("login")
-    public String gotoLoginPage(@RequestParam String name, ModelMap model) {
-        model.put("name", name);
-        logger.debug("DEBUG level - Request Param - name: {}", name);
-        logger.info("INFO level - Request Param - name: {}", name);
-        logger.warn("WARN level - Request Param - name: {}", name);
-
+    @RequestMapping(value="login", method= RequestMethod.GET)
+    public String gotoLoginPage() {
         return "login";
+    }
+
+    @RequestMapping(value="login", method= RequestMethod.POST)
+    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        model.put("name", name);
+        model.put("password", password); // for demo only
+
+        return "welcome";
     }
 }
