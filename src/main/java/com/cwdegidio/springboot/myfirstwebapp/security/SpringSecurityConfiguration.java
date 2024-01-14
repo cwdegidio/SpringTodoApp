b9f72480ff63023baadb507e4a21979bffa1fa91
Curtis W. DeGidio <curtis.degidio@gmail.com>
2024-01-14 16:09:34 -0600

    Updated pom for h2 and jpa, updated Spring Security for h2 implementation, updated Todo bean to act as entity, updated application.properties to defer database initialization, added seed data in data.sql

--- src/main/java/com/cwdegidio/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/cwdegidio/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,16 @@ package com.cwdegidio.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -38,5 +43,21 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // To deal with H2, we must disable CSRF
+    // and allow Frames
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+
+        http.formLogin(withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
     }
 }
